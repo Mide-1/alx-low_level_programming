d55e53a6e94f00e2d314176c055f984974673628
Mide-1 <olawuyiezekiel2070@gmail.com>
2022-09-13 17:19:01 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - function to pickk abc 10 times
+ * print_alphabet_x10 - function to print abc 10 times
  *
  * Return: 0
  */
 void print_alphabet_x10(void)
 {
-	char , i;
+	char c, i;
 
 	for (i = 0; i <= 9; i++)
 	{
